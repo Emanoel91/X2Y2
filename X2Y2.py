6249fcdfe58b1ec392079dc20526f778932d6750
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-04-26 17:02:09 +0330

    Update X2Y2.py

--- X2Y2.py
+++ X2Y2.py
@@ -18,7 +18,7 @@ st.title('Evaluating the Activities of X2Y2 Users')
 # Content
 c1, c2 = st.columns(2)
 
-c1.image(Image.open('Image/X2Y24.jpg'))
+c1.image(Image.open('Image/X2Y25.jpg'))
 
 
 st.subheader('📃 Introduction')
